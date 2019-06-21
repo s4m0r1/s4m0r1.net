f0692e11496f2f4f0d9724175fe91fb07ffe5cac
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-21 10:15:24 +0900

    コンソールでのエラー対応 使っていないdivタグをしました

--- layouts/header.vue
+++ layouts/header.vue
@@ -3,7 +3,6 @@
         <section class="container-fluid px-0">
             <b-navbar toggleble="lg" type="dark" variant="dark">
                 <b-navbar-brand href="/">s4m0r1.net</b-navbar-brand>
-                <b-navbar-toggle taget="nav-colloapse"></b-navbar-toggle>
                 <b-collaps id="nac-collapse" is-nav>
                     <b-navbar-nav>
                         <b-nav-item href="/about">About</b-nav-item>
