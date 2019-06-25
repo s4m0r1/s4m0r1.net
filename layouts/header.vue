6a6166deaa43db7baabacb8f631ac2f9941ad7f5
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-25 19:35:38 +0900

    レイアウト修正＆バッチ追加

--- layouts/header.vue
+++ layouts/header.vue
@@ -2,7 +2,7 @@
     <div>
         <section class="container-fluid px-0">
             <b-navbar toggleble="lg" type="dark" variant="dark">
-                <b-navbar-brand href="/">s4m0r1.net[ß]</b-navbar-brand>
+                <b-navbar-brand href="/">s4m0r1.net<span class="beta-badge">ß</span></b-navbar-brand>
                 <b-collaps id="nac-collapse" is-nav>
                     <b-navbar-nav>
                         <b-nav-item href="/about">About</b-nav-item>
@@ -21,3 +21,27 @@ export default {
     layout: 'header'
 }
 </script>
+
+<style>
+    .beta-badge,
+    .link-badge {
+        padding: 3px 10px;
+        margin-right: 8px;
+        margin-left: 10px;
+        font-size: 75%;
+        color: white;
+        border-radius: 6px;
+        box-shadow: 0 0 3px #ddd;
+        white-space: nowrap;
+    }
+
+    .link-badge {
+        background-color: #58ACFA;
+        /*青*/
+    }
+
+    .beta-badge {
+        background-color: rgb(112, 84, 194);
+        /*緑*/
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,44 @@
 <template>
-    <div class="jumbotron">
-        <h1 class="display-4">s4m0r1.net[ß]へようこそ</h1>
-        <p class="lead">無限にβが取れないサイト</p>
-        <hr class="my-2">
-        <p></p>
-        <p class="lead">
-            <a class="btn btn-primary btn-lg" href="/about" role="button">About</a>
-            <a class="btn btn-primary btn-lg" href="/product">Product</a>
-        </p>
+    <div class="container-fluid px-0">
+        <div class="jumbotron">
+            <h1 class="display-3">s4m0r1.net<span class="beta-badge">ß</span></h1>
+            <p class="lead"><a href="https://twitter.com/s4m0r1">さもり</a>の自宅サーバーでうごいてるアレ</p>
+            <hr class="my-2">
+            <p>現状β版なのでいろいろおかしくなったりしますごめんなさい</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="/about" role="button">About</a>
+            </p>
+        </div>
     </div>
 </template>
 <script>
-import header from '~/layouts/header.vue'
+    import header from '~/layouts/header.vue'
 
-export default {
-    layout: 'header'
-}
+    export default {
+        layout: 'header'
+    }
 </script>
 
 <style>
+    .beta-badge,
+    .link-badge {
+        padding: 3px 10px;
+        margin-right: 8px;
+        margin-left: 10px;
+        font-size: 75%;
+        color: white;
+        border-radius: 6px;
+        box-shadow: 0 0 3px #ddd;
+        white-space: nowrap;
+    }
+
+    .link-badge {
+        background-color: #58ACFA;
+        /*青*/
+    }
+
+    .beta-badge {
+        background-color: rgb(112, 84, 194);
+        /*緑*/
+    }
 </style>
