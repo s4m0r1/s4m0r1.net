17876b30ccd316f7f43f8985840dc33b6a14fc27
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-27 01:46:03 +0900

    一時的変更

--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,15 @@
 <template>
     <section class="container-fluid">
         <div class="row">
-            <b-alert show variant="success">
-                <h4 class="alert-heading">現在このページは作成中です</h4></b-alert>
-            </div>
-        <div class="row">
-            <div class="col-md col-xs-1 icon">
+            <div class="col-md-3 icon">
                 <img class="s4m0r1ico" v-b-popover.hover="{title:'s4m0r1', content:'雪歩好き！'}" src="https://avatars1.githubusercontent.com/u/29473019" alt="s4m0r1">
             </div>
-            <div class="col-md">
-                <div class="row">
+            <div class="me">
+                <div class="col-md-1">
                     <h2>Name: s4m0r1</h2>
+                </div>
+                <div class="col-md-1">
+                    <h2>Twitter: @s4m0r1</h2>
                 </div>
             </div>
         </div>
@@ -31,17 +30,14 @@ export default {
     justify-content: center;
     align-items: center;
 }
-.container-fluid {
-    /* background-color: blueviolet; */
+.me {
+    background-color: darkgray;
+
 }
-.row {
-    /* background-color: coral; */
-}
-.col-md-1 {
-    display: flex;
+.col-md-6 {
     justify-content: center;
     align-items: center;
-    background-color: brown;
+    /* background-color: brown; */
 }
 .icon {
     padding: 10%;
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,15 @@
     <div class="container-fluid px-0">
         <div class="jumbotron">
             <h1 class="display-4">s4m0r1.net <span class="beta-badge">ß</span></h1>
-            <p class="lead"><a href="https://twitter.com/s4m0r1">さもり</a>ねっと</p>
+            <p class="lead"><a href="https://twitter.com/s4m0r1">さもり</a>どっとねっと</p>
             <hr class="my-2">
             <p>現状β版なのでいろいろおかしくなったりします。 ごめんなさい</p>
             <p class="lead">
                 <a class="btn btn-primary btn-lg" href="/about" role="button">About</a>
             </p>
+            
         </div>
+
     </div>
 </template>
 <script>
