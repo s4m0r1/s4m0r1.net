ecdf6f441e44e4c20e521e18dda96cf8ffb173c5
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-27 02:30:30 +0900

    md確認まで行った

---
+++ pages/blog.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <v-layout column justify-center align-center>
+            <v-flex xs12 sma12 md12 style="width: 100%">
+                <div class="ma-4 markdown-body" v-html="$md.render(article.bodyContent)" />
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        async asyncData() {
+            const article = await import(`~/contents/json/2019-06-27-test002.json`)
+            return {
+                article: article
+            }
+        }
+    }
+</script>
