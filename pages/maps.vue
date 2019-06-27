996ccad62a0555a3d0a466278df7de8e79cfb129
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-27 10:50:11 +0900

    jsonを受け取って表示までやった

---
+++ pages/maps.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <v-layout column justify-center align-center>
+            <v-flex xs12 sma12 md12 style="width: 100%">
+                <div class="ma-4" v-html="JSON.stringify(sourceFile)" />
+                <p v-for="n in jsonAll.sourceFileArray.length">
+                    {{ n }}
+                </p>
+            </v-flex>
+        </v-layout>
+    </v-container>
+    <!-- <p>{{ jsonAll }}</p> -->
+</template>
+
+
+<script>
+import header from '~/layouts/header.vue'
+
+export default {
+    layout: 'header',
+    asyncData() {
+        return {
+            jsonAll:  process.env.summary
+        }
+    },
+}
+
+</script>
