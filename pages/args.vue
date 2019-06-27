db56cc106630765f2ad89949233337cfcd5f036f
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-27 10:49:56 +0900

    引数の検証

---
+++ pages/args.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>引数のデータ: {{hoge}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+    data: function() {
+        return {
+            hoge: '空'
+        }
+    },
+    asyncData(context) {
+    return {
+        hoge: context.query['hoge']
+    }
+    }
+}
+</script>
