ea27a4e76db92ad0d38e1c2f610a75169859c5e4
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2019-06-28 22:29:16 +0900

    確認用とルートページ雛形作成

---
+++ pages/memo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="jumbotron-flude">
+            <h1 class="display-3"> メモ集 </h1>
+            <p class="lead">Qiitaに上げるようなもんじゃないのを載せてます</p>
+            <hr class="my-2">
+            <p>More info</p>
+        </div>
+        <div class="row">
+        </div>
+    </div>
+</template>
+
+<script>
+import header from '~/layouts/header.vue'
+
+export default {
+    layout: 'header'
+}
+</script>
---
+++ pages/memo/001.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-html="test"></div>
+</template>
+
+<script>
+    import header from '~/layouts/header.vue'
+    import test from '~/assets/memo/test.md'
+
+    export default {
+        layout: 'header',
+        computed: {
+            test() {
+                return test
+            }
+        }
+    }
+</script>
